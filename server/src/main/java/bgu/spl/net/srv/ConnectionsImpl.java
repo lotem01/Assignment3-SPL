d8d713be812ad39d22f2bf179ec5a6c6fe90db51
lior4715 <lior4715@gmail.com>
2026-01-16 16:28:14 +0000

    fixed logic

--- server/src/main/java/bgu/spl/net/srv/ConnectionsImpl.java
+++ server/src/main/java/bgu/spl/net/srv/ConnectionsImpl.java
@@ -1,28 +1,30 @@
 package bgu.spl.net.srv;
 
 import java.io.IOException;
-import java.util.Set;
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class ConnectionsImpl<T> implements Connections<T> {
 
     private final ConcurrentHashMap<Integer, ConnectionHandler<T>> handlers = new ConcurrentHashMap<>();
 
-    private final ConcurrentHashMap<String, Set<Integer>> channelSubs = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<String, ConcurrentHashMap<Integer, String>> channelSubs = new ConcurrentHashMap<>();
+
+
 
     public void addConnection(int connectionId, ConnectionHandler<T> handler) {
         handlers.put(connectionId, handler);
     }
 
-    public void subscribe(int connectionId, String channel) {
-        if(!channelSubs.containsKey(channel)) {
-            channelSubs.put(channel, ConcurrentHashMap.newKeySet());
+    public void subscribe(int connectionId, String channel, String subId) {
+        if(!channelSubs.keySet().contains(channel)) {
+            channelSubs.put(channel, new ConcurrentHashMap<>());
         }
-        channelSubs.get(channel).add(connectionId);
+        channelSubs.get(channel).put(connectionId, subId);
     }
 
     public void unsubscribe(int connectionId, String channel) {
-        Set<Integer> subs = channelSubs.get(channel);
+        ConcurrentHashMap<Integer, String> subs = channelSubs.get(channel);
         if (subs != null) {
             subs.remove(connectionId);
             if (subs.isEmpty()) channelSubs.remove(channel, subs);
@@ -30,8 +32,8 @@ public class ConnectionsImpl<T> implements Connections<T> {
     }
 
     public boolean isSubscribed(int connectionId, String channel) {
-        Set<Integer> subs = channelSubs.get(channel);
-        return subs != null && subs.contains(connectionId);
+        ConcurrentHashMap<Integer, String> subs = channelSubs.get(channel);
+        return subs != null && subs.containsKey(connectionId);
     }
 
     @Override
@@ -49,17 +51,16 @@ public class ConnectionsImpl<T> implements Connections<T> {
 
     @Override
     public void send(String channel, T msg) {
-        Set<Integer> subs = channelSubs.get(channel);
+        Map<Integer, String> subs = channelSubs.get(channel);
         if (subs == null) return;
-        for (Integer id : subs) {
-            send(id, msg);
-        }
+        for (Integer id : subs.keySet()) send(id, msg);
     }
+
 
     @Override
     public void disconnect(int connectionId) {
         ConnectionHandler<T> h = handlers.remove(connectionId);
-        for (Set<Integer> subs : channelSubs.values()) {
+        for (ConcurrentHashMap<Integer, String> subs : channelSubs.values()) {
             subs.remove(connectionId);
         }
 
@@ -68,5 +69,9 @@ public class ConnectionsImpl<T> implements Connections<T> {
                 h.close();
             } catch (IOException ignored) {}
         }
+    }
+
+    public Map<Integer, String> getSubscribers(String channel) {
+        return channelSubs.get(channel);
     }
 }
